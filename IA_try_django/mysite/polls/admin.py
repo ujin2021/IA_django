b72a919e0952b06e05412b184c7a4e3a069eb42b
Leeyoujin <ujin2021@naver.com>
2020-05-19 00:24:55 +0900

    PART3

--- IA_try_django/mysite/polls/admin.py
+++ IA_try_django/mysite/polls/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
-from .models import Question
+from .models import Question, Choice
 
-admin.site.register(Question)
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3 # 입력 칸 추가(3단위로)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('question_text', 'pub_date')
+    list_filter = ['pub_date']
+    inlines = [ChoiceInline]
+
+admin.site.register(Question, QuestionAdmin)
