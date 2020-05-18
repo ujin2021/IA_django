153985e9185a224c009d086bd6f814f412415f67
Leeyoujin <ujin2021@naver.com>
2020-05-19 02:49:47 +0900

    PART7

--- IA_try_django/mysite/polls/admin.py
+++ IA_try_django/mysite/polls/admin.py
@@ -5,8 +5,12 @@ class ChoiceInline(admin.TabularInline):
     model = Choice
     extra = 3 # 입력 칸 추가(3단위로)
 class QuestionAdmin(admin.ModelAdmin):
-    list_display = ('question_text', 'pub_date')
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
     list_filter = ['pub_date']
+    search_fields = ['question_text']
+    fieldsets = [
+        (None,{'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
     inlines = [ChoiceInline]
-
 admin.site.register(Question, QuestionAdmin)
