8f14acc95cc459823fc8376ed7dd7bc93c4b3a07
ujin2021 <ujin2021@naver.com>
2020-05-29 22:38:11 +0900

    localllibrary_0529_2238

--- LOCAL_LIBRARY/locallibrary/catalog/admin.py
+++ LOCAL_LIBRARY/locallibrary/catalog/admin.py
@@ -1,3 +1,44 @@
 from django.contrib import admin
+from .models import Author, Genre, Book, BookInstance, Language
 
-# Register your models here.
+# admin.site.register(Book)
+# admin.site.register(Author)
+# admin.site.register(BookInstance)
+admin.site.register(Language)
+admin.site.register(Genre)
+
+@admin.register(Author) # admin.site.register(Author, AuthorAdmin)
+class AuthorAdmin(admin.ModelAdmin):
+    class BooksInline(admin.TabularInline):
+        model = Book
+
+    # fields to be displayed in list view - display할 때
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+    # orders in detail view - add할 때, detail view에서
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+    #inline addition of book in detail view
+    inlines = [BooksInline]
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    class BooksInstanceInline(admin.TabularInline):
+        model = BookInstance
+
+    list_display = ('title', 'author', 'display_genre')
+    inlines = [BooksInstanceInline]
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ('book', 'status', 'due_back', 'id')
+    # filters that will be displayed in sidebar
+    list_filter = ('status', 'due_back')
+    # grouping of fields into sections - in add view
+    # devide into 2 section (None, Availability)
+    fieldsets = (
+        (None, {    # no section title
+            'fields':('book', 'imprint', 'id')
+        }),
+        ('Availability', {  # section title
+            'fields':('status', 'due_back', 'borrower')
+        }),
+    )
--- LOCAL_LIBRARY/locallibrary/catalog/views.py
+++ LOCAL_LIBRARY/locallibrary/catalog/views.py
@@ -1,3 +1,50 @@
 from django.shortcuts import render
+from .models import Book, Author, BookInstance, Genre
 
-# Create your views here.
+def index(request):
+    """View function for home page of site."""
+
+    # Generate counts of some of the main objects
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+
+    # Available copies of books
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+    
+    # The 'all()' is implied by default.
+    num_authors = Author.objects.count()
+
+    #Number of visits to this view, as counted in the session variable. 
+    num_visits = request.session.get
+
+    context = {
+        'num_books' : num_books,
+        'num_instances' : num_instances,
+        'num_instances_available' : num_instances_available,
+        'num_authors' : num_authors,
+    }
+
+    # Render the HTML template index.html with the data in the context variable. 
+    return render(request, 'index.html', context=context)
+
+from django.views import generic
+
+class BookListView(generic.ListView):
+    model = Book
+    # context_object_name = 'book_list'
+    # queryset = Book.objects.all()
+    # template_name = 'book_list.html'
+    paginate_by = 10
+
+class BookDetailView(generic.DetailView):
+    model = Book
+
+class AuthorListView(generic.ListView):
+    """Generic class-based list view for a list of authors."""
+    model = Author
+    paginate_by = 10
+
+
+class AuthorDetailView(generic.DetailView):
+    """Generic class-based detail view for an author."""
+    model = Author
